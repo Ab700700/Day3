880039e792e4a1e5f851de339c379a5378fcb350
Ab700700 <148686321+Ab700700@users.noreply.github.com>
2023-10-24 20:20:48 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,115 @@
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        //1.Write a program that checks the role of the user
+        //If the role is admin print "welcome admin"
+        //If the role is superuser print "welcome superuser"
+        //If the role is user print "welcome user"
+
+//        String role = "Admin";
+//        if(role.equalsIgnoreCase("admin")) System.out.println("Welcome admin");
+//        else if(role.equalsIgnoreCase("superuser")) System.out.println("Welcome superuser");
+//        else System.out.println("Welcome user");
+
+        //2.Take three numbers from the user and print the greatest number.
+        //Test Data
+        //Input the 1st number: 25
+        //Input the 2nd number: 78
+        //Input the 3rd number: 87
+
+
+//        int num1,num2,num3, theGreatest;
+//        System.out.print("Enter the first number: ");
+//         num1 = scan.nextInt();
+//        System.out.print("Enter the second number: ");
+//         num2 = scan.nextInt();
+//        System.out.print("Enter the third number: ");
+//        num3 = scan.nextInt();
+//
+//        if(num1>num2 && num1>num3) theGreatest = num1;
+//        else if (num2> num1 && num2 > num3) theGreatest = num2;
+//        else theGreatest = num3;
+//
+//            System.out.printf("The greatest: %d ",theGreatest);
+
+
+
+        //3.Write a Java program that keeps a number from the user and
+        //generates an integer between 1 and 7 and displays the name of the
+        //weekday.
+
+
+//
+//        int num = (int) (Math.random()*(6))+1;
+//
+//        switch (num){
+//            case 1:
+//                System.out.println("Sunday");
+//                break;
+//            case 2:
+//                System.out.println("Monday");
+//                break;
+//            case 3:
+//                System.out.println("Tuesday");
+//                break;
+//            case 4:
+//                System.out.println("Wednesday");
+//                break;
+//            case 5:
+//                System.out.println("Thursday");
+//                break;
+//            case 6:
+//                System.out.println("Friday");
+//                break;
+//            case 7:
+//                System.out.println("Saturday");
+//                break;
+//        }
+
+        
+        // 4-Write a program that takes a numeric score as input and prints the
+        //corresponding letter grade using the following grading scale:
+        //A: 90-100
+        //B: 80-89
+        //C: 70-79
+        //D: 60-69
+        //F: 0-59
+
+
+//        System.out.print("Enter your numeric score: ");
+//        int score = scan.nextInt();
+//        char grade ;
+//        if(score>=90 && score <= 100) grade = 'A';
+//        else if(score>=80 && score <= 89) grade='B';
+//        else if(score>= 70 && score <= 79) grade = 'C';
+//        else if(score >= 60 && score <= 69) grade = 'D';
+//        else grade = 'F';
+//
+//        System.out.printf("Numeric Score: %d \nLetter Grade: %c",score,grade);
+
+
+
+
+        //5 - Write a Java program that takes a person's age as input and
+        //categorizes them into one of three age categories: "Child," "Teenager,"
+        //or "Adult" using an if statement.
+
+//        System.out.print("Enter your age: ");
+//        int age = scan.nextInt();
+//        if(age>=20) System.out.println("You are an Adult");
+//        else if(age>=19 && age<=13) System.out.println("You are a Teenager");
+//        else System.out.println("You are a Child");
+
+
+
+
+
+
+
+    }
+}
